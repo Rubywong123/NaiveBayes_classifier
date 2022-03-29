e871db4a00eb9a7fe16a5be9e43565a1e427e042
Rubywong <75187575+Rubywong123@users.noreply.github.com>
2022-03-29 22:42:40 +0800

    custom stopwords list.

--- cleandata.py
+++ cleandata.py
@@ -5,10 +5,19 @@ import re
 
 
 punct = "/-?!.,#$%()*+-/:;<=>@[\\]^_`{|}~`" + '""“”’' + '∞θ÷α•à−β∅³π‘₹´°£€\×™√²—–&\n'
+custom_stoplist = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours',
+     'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their',
+      'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being',
+    'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the','because', 'as', 'until', 'at', 'by', 'for', 'with', 'about', 'against', 'between',
+     'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from' ,'then', 'once', 'here', 'there','each', 'few', 'other']
 
 def total_cleaning(texts):
+    
     nltk.download('stopwords')
-    stop_list = stopwords.words('english')
+    nltk.download('wordnet')
+    #stop_list = stopwords.words('english')
+    print('start cleaning')
+    stop_list = custom_stoplist
     stop_list.append('\'s')
     lemmatizer = WordNetLemmatizer()
     #print(stop_list)
@@ -30,5 +39,8 @@ def total_cleaning(texts):
 
     return cleaned_texts
 
+if __name__ == '__main__':
+    stop_list = stopwords.words('english')
+    print(stop_list)
 
 
